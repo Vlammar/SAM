56359ae9e135fa9af1a456edc36702c650a7ef58
root <root@Pc-valentin.localdomain>
2020-01-11 09:39:49 +0100

    ROC init

--- Projet2/test.py
+++ Projet2/test.py
@@ -1,14 +1,14 @@
 """"
-audio : MFCC aggrégé + régression logistique (variante séance 3)
-audio : MFCC + CNN (séance 4)
-audio : représentations déjà apprises + couche de sortie (séance 4)
-image : hog + régression logistique (séance 3)
-image : ResNet (séance 4)
+audio : MFCC aggrege + regression logistique (variante seance 3)
+audio : MFCC + CNN (seance 4)
+audio : representations deja apprises + couche de sortie (seance 4)
+image : hog + regression logistique (seance 3)
+image : ResNet (seance 4)
 """
 
 
 #TODO renommer le fichier
-import cv2
+#import cv2
 import numpy as np
 def resize(img,output_shape):#used for ResNet
     img_shape=img.shape
@@ -17,9 +17,20 @@ def resize(img,output_shape):#used for ResNet
         return img
 
 
-    img = cv2.resize(img, dsize=output_shape, interpolation=cv2.INTER_CUBIC)
+   # img = cv2.resize(img, dsize=output_shape, interpolation=cv2.INTER_CUBIC)
 
     return img
 
 def batch():
     pass
+import sklearn
+from sklearn.metrics import roc_curve, auc
+print("hello")
+y = np.array([1, 1, 2, 2])
+scores = np.array([0.1, 0.4, 0.35, 0.8])
+fpr, tpr, thresholds = sklearn.metrics.roc_curve(y, scores, pos_label=2)
+import matplotlib.pyplot as plt
+plt.plot(fpr)
+plt.plot(tpr)
+plt.show()
+print(thresholds)
